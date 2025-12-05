80a11ed001f7b82b55c4b94561dd04c76b2d9821
unbounddev <unbounddev@git>
2025-12-05 15:34:40 -0800

    [nvim] Added nvim-cmp

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -186,7 +186,6 @@ local function setup_lsp()
 		end,
 	})
 end
-setup_lsp()
 
 require("nvim-tree").setup({
   filters = {
@@ -235,6 +234,58 @@ require("themery").setup({
 })
 
 require("gitsigns").setup({})
--- TODO: Add nvim-cmp
+
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, 
+  }, 
+  {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lsps = { "ts_ls", "pyright" }
+for index, lsp in ipairs(lsps) do
+  vim.lsp.config(lsp, {
+    capabilities = capabilities
+  })
+end
+
+setup_lsp()
 -- NOTE: Think about adding which-key.nvim
 -- NOTE: Look at https://erock-git-dotfiles.pgs.sh/tree/main/item/dot_config/nvim/init.lua.html
